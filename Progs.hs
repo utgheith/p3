96a656d9b27e2594cbfa5352bb8723c52c2a05d2
Amerikrainian <olegjpittman@gmail.com>
2025-10-14 09:43:52 -0500

    Rewrite progs using op overloads

--- Progs.hs
+++ Progs.hs
@@ -1,57 +1,41 @@
 module Progs (prog, prog2, prog3) where
 
-import qualified Term as T
+import Term
 
-prog :: T.Term
+infixl 1 ~
+(~) :: Term -> Term -> Term
+(~) = Seq
+
+infixl 9 <=>
+(<=>) :: String -> Term -> Term
+(<=>) = Let
+
+prog :: Term
 prog =
-  T.Seq
-    (T.Let "x" (T.Literal 10))
-    ( T.While
-        (T.Var "x")
-        ( T.Seq
-            (T.Write (T.Var "x"))
-            (T.Let "x" (T.Sub (T.Var "x") (T.Literal 1)))
-        )
+  "x" <=> Literal 10 ~
+  While
+    (Var "x")
+    ( Write (Var "x") ~
+      "x" <=> Sub (Var "x") (Literal 1)
     )
 
-prog2 :: T.Term
-prog2 = T.Seq (T.Write (T.Var "x")) (T.Write (T.Literal 42))
+prog2 :: Term
+prog2 = Write (Var "x") ~ Write (Literal 42)
 
-prog3 :: T.Term
+prog3 :: Term
 prog3 =
-  T.Seq
-    (T.Let "x" (T.Literal 10))
-    ( T.Seq
-        (T.Let "y" (T.Literal 5))
-        ( T.Seq
-            (T.Write (T.Gt (T.Var "x") (T.Var "y")))
-            ( T.Seq
-                (T.Write (T.Lt (T.Var "x") (T.Var "y")))
-                ( T.Seq
-                    (T.Write (T.Eq (T.Var "x") (T.Var "y")))
-                    ( T.Seq
-                        (T.Write (T.Neq (T.Var "x") (T.Var "y")))
-                        ( T.Seq
-                            (T.Write (T.And (T.Gt (T.Var "x") (T.Var "y")) (T.Neq (T.Var "x") (T.Var "y"))))
-                            ( T.Seq
-                                (T.Write (T.Or (T.Lt (T.Var "x") (T.Var "y")) (T.Gt (T.Var "x") (T.Var "y"))))
-                                ( T.Seq
-                                    (T.Write (T.Not (T.Lt (T.Var "x") (T.Var "y"))))
-                                    ( T.Seq
-                                        (T.If (T.Gt (T.Var "x") (T.Var "y")) (T.Write (T.Literal 100)) (T.Write (T.Literal 200)))
-                                        ( T.While
-                                            (T.Gt (T.Var "y") (T.Literal 0))
-                                            ( T.Seq
-                                                (T.Write (T.Var "y"))
-                                                (T.Let "y" (T.Sub (T.Var "y") (T.Literal 1)))
-                                            )
-                                        )
-                                    )
-                                )
-                            )
-                        )
-                    )
-                )
-            )
-        )
+  "x" <=> Literal 10 ~
+  "y" <=> Literal 5 ~
+  Write (Gt (Var "x") (Var "y")) ~
+  Write (Lt (Var "x") (Var "y")) ~
+  Write (Eq (Var "x") (Var "y")) ~
+  Write (Neq (Var "x") (Var "y")) ~
+  Write (And (Gt (Var "x") (Var "y")) (Neq (Var "x") (Var "y"))) ~
+  Write (Or (Lt (Var "x") (Var "y")) (Gt (Var "x") (Var "y"))) ~
+  Write (Not (Lt (Var "x") (Var "y"))) ~
+  If (Gt (Var "x") (Var "y")) (Write (Literal 100)) (Write (Literal 200)) ~
+  While
+    (Gt (Var "y") (Literal 0))
+    ( Write (Var "y") ~
+      "y" <=> Sub (Var "y") (Literal 1)
     )
