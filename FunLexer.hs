96ac49490bd9290cfa8a68f96fcee8baa6402705
Yifan Ma <yifanma07@gmail.com>
2025-10-15 17:51:17 -0500

    String literals parsing in lexer

--- FunLexer.hs
+++ FunLexer.hs
@@ -9,6 +9,7 @@ data Token
   | Ident String
   | Keyword String
   | Symbol String
+  | StringLiteral String
   | Error String
   deriving (Show, Eq)
 
@@ -44,6 +45,12 @@ lexer = unfoldr step
       | isAlpha c =
           let (var, rest) = span isAlphaNum s
            in Just (if S.member var keywords then Keyword var else Ident var, rest)
+    -- string literals
+    step ('"' : rest) =
+      let (str, rest2) = span (/= '"') rest
+       in case rest2 of
+            ('"' : rest3) -> Just (StringLiteral str, rest3)
+            _ -> Just (Error "Unclosed string literal", "")
     -- symbols
     step (c : rest)
       | S.member c symbols =
