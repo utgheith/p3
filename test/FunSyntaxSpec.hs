5e42f36e834612606523682d504f840a06d89cee
Emerald-Z <Emerald-Z@users.noreply.github.com>
2025-10-20 00:41:16 +0000

    automated ormolu commit

--- test/FunSyntaxSpec.hs
+++ test/FunSyntaxSpec.hs
@@ -1,10 +1,10 @@
 module FunSyntaxSpec (spec) where
 
-import Test.Hspec
+import FunLexer (Token (..))
 import FunSyntax (parse, prog)
 import ParserCombinators (eof)
-import Term (Term(..), BinaryOp(..))
-import FunLexer (Token(..))
+import Term (BinaryOp (..), Term (..))
+import Test.Hspec
 
 -- Helper function to parse from a string
 parseString :: String -> Either String (Term, [Token])
@@ -156,27 +156,31 @@ spec = do
 
     it "parses from test files" $ do
       let testFiles = ["test1.fun", "test2.fun", "simple.fun"]
-      mapM_ (\file -> do
-        result <- parseFile file
-        case result of
-          Left err -> fail $ "Failed to parse " ++ file ++ ": " ++ err
-          Right (ast, _) -> do
-            ast `shouldSatisfy` (const True)  -- Just check it parses successfully
-        ) testFiles
+      mapM_
+        ( \file -> do
+            result <- parseFile file
+            case result of
+              Left err -> fail $ "Failed to parse " ++ file ++ ": " ++ err
+              Right (ast, _) -> do
+                ast `shouldSatisfy` (const True) -- Just check it parses successfully
+        )
+        testFiles
 
   describe "File-based tests" $ do
     it "can parse complex programs from files" $ do
-      let complexProgram = unlines [
-            "var x = 10",
-            "fun f(n) {",
-            "    return n * 2",
-            "}",
-            "y = f(x)",
-            "if (y > 15) {",
-            "    print y",
-            "} else {",
-            "    print 0",
-            "}"]
+      let complexProgram =
+            unlines
+              [ "var x = 10",
+                "fun f(n) {",
+                "    return n * 2",
+                "}",
+                "y = f(x)",
+                "if (y > 15) {",
+                "    print y",
+                "} else {",
+                "    print 0",
+                "}"
+              ]
       let result = parseString complexProgram
       case result of
         Left err -> fail $ "Parse error: " ++ err
